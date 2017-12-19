b23f3dead8b98c7bee1537a31b8740bc70c4408b
facciod <leonardofdemo@gmail.com>
2017-12-19 06:47:51 -0200

    Update sampleTest.js

--- tests/functional/scripts/sampleTest.js
+++ tests/functional/scripts/sampleTest.js
@@ -4,8 +4,8 @@ module.exports = {
       .url('http://google.com')
       .waitForElementVisible('input.gsfi', 60000, true,
         function(){}, 'Waiting for Search Input to become visible')
-      .setValue('input.gsfi', 'Test')
-      .waitForElementPresent('#search-page', 60000)
+      .setValue('input.gsfi', ['functional tests', browser.Keys.ENTER])
+      .waitForElementPresent('#searchform', 60000)
       .pause(5000)
       .end();
   }
